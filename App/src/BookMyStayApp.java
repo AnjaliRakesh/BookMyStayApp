53a60bddf97c6fd603139cf3d22f157df45bfee6
Anjali <ar7361@srmist.edu.in>
2026-03-05 11:18:49 +0530

    Added UC1 Application Entry & Welcome Message

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,20 @@
+/**
+ * BookMyStayApp
+ * Hotel Booking Management System
+ * 
+ * @author Anjali
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("     Welcome to Book My Stay App    ");
+        System.out.println("         Version 1.0                ");
+        System.out.println("====================================");
+
+    }
+
+}
